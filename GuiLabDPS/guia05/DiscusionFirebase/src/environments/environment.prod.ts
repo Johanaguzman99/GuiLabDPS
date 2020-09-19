a42a3ba0d1087d4216a0b9f8cb4253ad35910728
estela801 <ros98cg98@gmail.com>
2020-09-19 00:07:11 -0600

    Ya esta subido al host:)

--- GuiLabDPS/guia05/DiscusionFirebase/src/environments/environment.prod.ts
+++ GuiLabDPS/guia05/DiscusionFirebase/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyADjvLKs9HaBCflnDaaDapHTAIK6c7IW5E",
+    authDomain: "alumno-dps.firebaseapp.com",
+    databaseURL: "https://alumno-dps.firebaseio.com",
+    projectId: "alumno-dps",
+    storageBucket: "alumno-dps.appspot.com",
+    messagingSenderId: "693727460029",
+    appId: "1:693727460029:web:24f3285213e3c55a5b15d4",
+    measurementId: "G-M2CYCB77WM"
+  }
 };
