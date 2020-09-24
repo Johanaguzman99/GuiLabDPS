30b57b60be2328b374b5c20749ce60b1a14f69e9
estela801 <ros98cg98@gmail.com>
2020-09-23 21:45:24 -0600

    Primer commit guia6

---
+++ GuiLabDPS/guia06/AutAngular/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{ apiKey: "AIzaSyD8FCMAIF1uP-LkQErlcSEFs9u2lQj6Kkk",
+  authDomain: "autenticacion-fed11.firebaseapp.com",
+  databaseURL: "https://autenticacion-fed11.firebaseio.com",
+  projectId: "autenticacion-fed11",
+  storageBucket: "autenticacion-fed11.appspot.com",
+  messagingSenderId: "216354445940",
+  appId: "1:216354445940:web:71b4b66431a97c1f0a0f21",
+  measurementId: "G-E0EB1WZDS3"}
+};
